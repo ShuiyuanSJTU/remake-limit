31ca20bdb525526af4bc294d735ac8992bf4b413
Jiajun Du <jiajun_du@outlook.com>
2022-11-26 16:13:35 +0800

    feat: update rule

--- plugin.rb
+++ plugin.rb
@@ -17,20 +17,21 @@ after_initialize do
   class ::UsersController
 
     before_action :check_remake_limit, only: [:create]
+    after_action :clean_silence_diff, only: [:create]
+
+    before_action :add_remake_limit, only: [:destroy]
+
+    private
 
     def check_remake_limit
       if SiteSetting.remake_limit_enabled
         remake_at = ::PluginStore.get(PLUGIN_NAME, params[:email])
         if remake_at
           time = Time.parse(remake_at) + SiteSetting.remake_limit_period * 86400
-          if Time.now < time
-            render json: { success: false, message: "您正处于转生限制期，请于#{time.strftime("%Y-%m-%d %H:%M:%S")}之后再注册！" }
-            return
+          silence_diff = ::PluginStore.get(REMAKE_LIMIT_FOR_SILENCED, params[:email])
+          if silence_diff # 补足禁言时长
+            time = time + silence_diff
           end
-        end
-        silence_till = ::PluginStore.get(REMAKE_LIMIT_FOR_SILENCED, params[:email])
-        if silence_till
-          time = Time.parse(silence_till)
           if Time.now < time
             render json: { success: false, message: "您正处于转生限制期，请于#{time.strftime("%Y-%m-%d %H:%M:%S")}之后再注册！" }
           end
@@ -38,7 +39,9 @@ after_initialize do
       end
     end
 
-    before_action :add_remake_limit, only: [:destroy]
+    def clean_silence_diff
+      ::PluginStore.remove(REMAKE_LIMIT_FOR_SILENCED, params[:email])
+    end
 
     def add_remake_limit
       if SiteSetting.remake_limit_enabled
@@ -46,8 +49,11 @@ after_initialize do
         guardian.ensure_can_delete_user!(@user)
         key = @user.email
         ::PluginStore.set(PLUGIN_NAME, key, Time.now)
-        if @user.silenced?
-          ::PluginStore.set(REMAKE_LIMIT_FOR_SILENCED, key, @user.silenced_till)
+        if @user.silenced? # 补足禁言时长
+          diff = (@user.silenced_till - Time.now).to_i
+          if diff > 0
+            ::PluginStore.set(REMAKE_LIMIT_FOR_SILENCED, key, diff)
+          end
         end
       end
     end
