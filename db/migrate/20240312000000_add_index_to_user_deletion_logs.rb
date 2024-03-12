218e786e803c58e8b74dd8a137ee945e41108a97
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-03-12 22:53:19 +0800

    db: add index

---
+++ db/migrate/20240312000000_add_index_to_user_deletion_logs.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddIndexToUserDeletionLogs < ActiveRecord::Migration[7.0]
+    def change
+        add_index :user_deletion_logs, :email
+        add_index :user_deletion_logs, :jaccount_name
+        add_index :user_deletion_logs, :jaccount_id
+    end
+end
