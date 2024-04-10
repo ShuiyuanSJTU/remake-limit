efb61819ec700c0ce8d513675439650596ad4597
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2024-04-10 23:33:32 +0800

    chore: bump version

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: remake-limit
 # about: limit user remake frequency
-# version: 0.1.0
+# version: 0.2.0
 # authors: dujiajun,pangbo
 # url: https://github.com/ShuiyuanSJTU/remake-limit
 # required_version: 2.7.0
